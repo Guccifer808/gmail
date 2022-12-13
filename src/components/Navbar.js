6d68d8793d6c8cad11f6ada6ce5209969a0aa091
G808 <hucciallstars@gmail.com>
2022-12-14 01:14:55 +0200

    Navbar edits

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,54 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import { FiMenu, FiSearch } from "react-icons/fi";
+import { TbGridDots } from "react-icons/tb";
+import profilepic from "../assets/profilepic1.jpg";
 const Navbar = () => {
   return (
     <div className="flex items-center justify-between border-b py-3 px-6">
       {/* L Section */}
-      <div className="flex items-center gap-3">
-        <FiMenu className="w-6 h-6 text-gray-500" />
-        <div className="w-14 h-[2.5rem] flex ">
-          <img src={logo} alt="logo" className="h-10 object-cover "></img>
+      <div className="flex items-center gap-3 ">
+        <FiMenu className="w-6 h-6 text-gray-500 hover:text-gray-900 cursor-pointer" />
+        <div className="w-14 h-[2.5rem] flex">
+          <img
+            src={logo}
+            alt="logo"
+            className="h-10 object-cover cursor-pointer"
+          ></img>
         </div>
-        <p className="-ml-3 text-gray-500 text-[22px] font-semibold">Gmail</p>
+        <p className="-ml-3 text-gray-500 text-[22px] font-semibold cursor-pointer">
+          Gmail
+        </p>
       </div>
       {/* Search input */}
       <div className="w-full">
         <div className="flex relative items-center mx-auto max-w-[25rem]">
           <input
             type="search"
-            className="py-1.5 px-3 rounded-full bg-neutral-100 w-full pl-8 hover:bg-neutral-200/70 focus:outline-neutral-300"
+            className="py-1.5 px-3 rounded-full bg-neutral-100 w-full pl-12 hover:bg-neutral-200/70 outline-0"
             placeholder="Search mail"
           />
-          <FiSearch className="absolute text-[20px] left-2" />
+          <div className="absolute text-[20px] left-0 hover:bg-gray-300/50 rounded-l-full p-2">
+            <FiSearch className="pl-1 cursor-pointer" />
+          </div>
         </div>
       </div>
       {/* R Section */}
-      <div className=""></div>
+      <div className="flex items-center gap-3">
+        <div className="flex items-center gap-2 border p-1.5 rounded-full hover:bg-gray-200/70 cursor-pointer">
+          <div className="w-8 h-8 flex rounded-full bg-gray-300 ">
+            <img
+              src={profilepic}
+              alt="profilepic"
+              className="object-cover rounded-full"
+            />
+          </div>
+          <p className="whitespace-nowrap font-semibold text-gray-500 text-sm pr-1">
+            Serhii D.
+          </p>
+        </div>
+        <TbGridDots className="w-6 h-6 text-gray-500 hover:text-gray-900 cursor-pointer" />
+      </div>
     </div>
   );
 };
